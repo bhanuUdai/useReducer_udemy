993ea55162097092d1789d6ca789349d40da9716
bhanu <udaibhanu2602@gmail.com>
2022-09-28 23:35:01 +0530

    Context Hook Task-9(2)

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,38 +1,39 @@
-import React from 'react';
+import React,{useContext} from 'react';
 
 import classes from './Navigation.module.css';
 import AuthContext from '../../store/auth-context';
 const Navigation = (props) => {
+
+     const ctx= useContext(AuthContext)
+
   return (
-    <AuthContext.Consumer>
-      {(ctx)=>
-      {
-        return ( <nav className={classes.nav}>
-          <ul>
-            {ctx.isLoggedIn && (
-              <li>
-                <a href="/">Users</a>
-              </li>
-            )}
-            {ctx.isLoggedIn && (
-              <li>
-                <a href="/">Admin</a>
-              </li>
-            )}
-            {ctx.isLoggedIn && (
-              <li>
-                <button onClick={props.onLogout}>Logout</button>
-              </li>
-            )}
-          </ul>
-        </nav>)
-      }}
-       
-    </AuthContext.Consumer>
-    
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
 export default Navigation;
 
-//ctx is used in place of props, before isLoggedIn
+
+
+//now we are using useContext hook, which is better then ".Consumer",
+//Here we import useContext
+//Then use below in function i.e   const ctx=   useContext(AuthContext)
+//in it we pass context object name which we want to use here in component
